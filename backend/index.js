3b6b78f13f9ff82a0945cd125280ff6a85afb4be
rojinraj641 <rojinraj96@gmail.com>
2025-03-20 19:20:44 +0530

    error handling and response handling

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,15 @@
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import app from "./app.js"
 
 
 dotenv.config({path: "./.env"});
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 3000,()=>{
+        console.log(`Server is running on ${process.env.PORT}`);
+    });
+})
+.catch((error)=>{
+    console.log(`MongoDB connection failed !!!`,error);
+})
